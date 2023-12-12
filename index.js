01d0a7680b82475c25a906ac1619284713957d72
Darllan <darllanvieira6@gmail.com>
2023-12-11 21:20:41 -0300

    Primeiros passos

---
+++ index.js
@@ -0,0 +1,6 @@
+const saldoVitorias = "Vitoria";
+const saldoNivel = "Derrota";
+
+if (saldoVitorias >= 0 && saldoVitorias <= 10){
+    console.log(`${saldoVitorias} esta no nivel ${saldoNivel} ele é Ferro.`);
+}
