c5a88c7f6cb648b20d0772e85ed1fbd49b62235f
Darllan <darllanvieira6@gmail.com>
2023-12-11 22:01:20 -0300

    Finalizando

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const saldoVitorias = "Vitoria";
-const saldoNivel = "Derrota";
+function calcularNivel(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
 
-if (saldoVitorias >= 0 && saldoVitorias <= 10){
-    console.log(`${saldoVitorias} esta no nivel ${saldoNivel} ele é Ferro.`);
+    let nivel = " ";
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    console.log(`O Herói tem um saldo de ${saldoVitorias} e esta no nivel de ${nivel}`);
 }
+
+calcularNivel(70,5);
